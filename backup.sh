b72b5a56e872cc583bd5ace7e55a2ce901855920
Martín <martin@aflotemx.com>
2024-02-27 08:51:59 -0600

    fix route

--- backup.sh
+++ backup.sh
@@ -3,11 +3,11 @@
 # prepare cloud remote path
 [ -n "$1" ] && cloud="$1"
 
-# loop git projects. 
-for git_project in */
+# loop git projects.
+for git_project in code/*/
 do
 # sync git project
-rclone -P sync $git_project $cloud/$git_project \
+rclone sync $git_project $cloud/$git_project \
 --exclude-from $git_project.gitignore \
-# --log-level ERROR --log-file backup_error.log
+--log-level ERROR --log-file code/backup_error.log
 done
